22d73988e3f587550ba22e121000f7dda3b63ec9
beggsfinest00 <81796822+beggsfinest00@users.noreply.github.com>
2023-04-19 15:18:44 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_Dallas
+{
+    class Game
+    {
+        //makes a baking feild of title and genre
+        public string title, genre;
+
+        //sets title to unknown title, sets genre to casual
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual") 
+        {
+            //makes title equal to prvious asignment 
+            title = titleParam;
+            //makes genre equal to prvious asignment
+            genre = genreParam;
+        }
+    }
+}
